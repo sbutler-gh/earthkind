0a04eafb3b39c17060e545779ac39b74c77ed192
sbutler-gh <sam@ysli.be>
2021-11-13 17:39:30 +0100

    Adding static adapter for cloudflare deploymnet

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+import adapter from '@sveltejs/adapter-static';
+
+export default {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
-
-export default config;
