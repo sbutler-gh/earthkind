44c9315cb07db7d2ceb92414ed0720e742aea4fb
sbutler-gh <sam@ysli.be>
2021-12-07 10:32:36 +0100

    Framework for LinkedIn 3-legged auth, auth currently not working (CORS error?)

---
+++ src/lib/variables.js
@@ -0,0 +1,9 @@
+//   export default variables = {
+//     linkedInClientID: import.meta.vite.LINKEDIN_CLIENT_ID
+//   };
+
+  let variables = {
+    linkedInClientID: import.meta.env.VITE_LINKEDIN_CLIENT_ID
+  };
+
+export default variables;
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,28 @@
+<script>
+import variables from "$lib/variables";
+
+
+    // import LinkedIn from 'passport-linkedin-oauth2';
+    // var LinkedInStategy = LinkedIn.Strategy;
+
+
+async function sendrequest() {
+
+    const response = await fetch(`https://serene-journey-42564.herokuapp.com/https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${variables.linkedInClientID}&redirect_uri=http://localhost:3000/auth/linkedin&state=foobar&scope=r_liteprofile%20r_emailaddress%20w_member_social`, {
+            method: 'GET',
+            // headers: myHeaders
+            })
+
+            console.log(response);
+
+            // let result = await response.json();
+            // console.log(result);
+}
+</script>
+
 <h1>Earthkind — Graphme</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click={sendrequest}>click</button>
+
+<a href="https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id={variables.linkedInClientID}&redirect_uri=https://127.0.0.1:3000/auth/linkedin/callback&state=true&scope=r_liteprofile%20r_emailaddress%20w_member_social">linkedin</a>
